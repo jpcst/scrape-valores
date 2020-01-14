242ea52aabee351cd3a2564e28db70a8d26ee3ed
jpcst <jpcst@pm.me>
2020-01-13 22:08:37 -0300

    Update Tg-Ub.py

--- Tg-Ub.py
+++ Tg-Ub.py
@@ -5,6 +5,7 @@ import requests
 
 dirId = '/home/jpcst/scrape-valores/id.txt'
 dirBot = '/home/jpcst/scrape-valores/bot.txt'
+dirIni = '/home/jpcst/scrape-valores/inicio.txt'
 
 with open(dirBot, 'r') as f, open(dirId, 'r') as g:
     api_bot = f.read()
@@ -12,9 +13,9 @@ with open(dirBot, 'r') as f, open(dirId, 'r') as g:
 
 v = scpr3.main()
 
-list = '——————————\n' + \
+list = '————————————————————\n' + \
         (','.join(map(str,v))).replace(',','\n') + \
-        '\n——————————'
+        '\n————————————————————'
 
 url = 'https://api.telegram.org/bot'+ api_bot + '/sendMessage?chat_id=' + chat_id + '&text=' + list
 go = requests.post(url)
